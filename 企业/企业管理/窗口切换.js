92a572d637a1cf8390e71b9075273dc8a3d03e80
icer1290 <ruijianbai19@gmail.com>
2022-05-12 19:10:24 +0800

    添加了人才资源相关内容

--- 企业/企业管理/窗口切换.js
+++ 企业/企业管理/窗口切换.js
@@ -5,4 +5,7 @@ window.onload = function(){
 	document.getElementById('postJob').onclick = function(){
 		document.getElementById('iframe').src = '企业职位-选择.html';
 	}
+	document.getElementById('humanResou').onclick = function(){
+		document.getElementById('iframe').src = '企业人才资源-1.html';
+	}
 }
